4617b8a4345e76fea5825c5c61fe181ba1c8ff7d
Adrien <adrien.favez@gmail.com>
2022-01-28 04:36:18 +0100

    Ajout d'une file d'attente
    Les gens attendent maintenant dehors si y'a pas de place dedans
    (attention à l'hécatombe...........)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -12,6 +12,10 @@ public class GameController : MonoBehaviour {
 	[SerializeField] private GameObject[] WelcomeSeats;
 	[SerializeField] private GameObject Patient;
 	[SerializeField] private float spawnRate;
+	[SerializeField] private Transform PatientQueueParent;
+	[SerializeField] private int patientQueueSize;
+
+	private GameObject[] PatientQueue;
 
 	private float elapsedTime, currentSpawnRate;
 
@@ -30,6 +34,7 @@ public class GameController : MonoBehaviour {
 		score = 0;
 		multiplicator = 1;
 		scoreText.text = "0";
+		PatientQueue = new GameObject[patientQueueSize];
 	}
 
 	private void Update() {
@@ -37,30 +42,83 @@ public class GameController : MonoBehaviour {
 
 		if (isPlaying) {
 			if (elapsedTime > currentSpawnRate) {
-				GameObject emptySeat = null;
-				// Looking for an empty welcome seat
-				for (int i = 0; i < WelcomeSeats.Length; i++) {
-					if (!WelcomeSeats[i].GetComponent<SeatController>().isHolding) {
-						emptySeat = WelcomeSeats[i];
-						break;
-					}
-				}
-
-				// An empty welcome seat was found
-				if (emptySeat != null) {
-					GameObject newPatient = Instantiate(Patient, emptySeat.transform.position, emptySeat.transform.rotation);
-					newPatient.name = "patient " + counter++;
-					newPatient.GetComponent<PatientController>().Initialize(this);
-					if (!emptySeat.GetComponent<SeatController>().ReceiveHold(newPatient)) {
-						// If a problem occurs, delete the patient
-						Destroy(newPatient);
-					}
+				if (TrySpawnNewPatient())
 					elapsedTime -= currentSpawnRate;
-				}
-
 			} else
 				elapsedTime += Time.deltaTime;
 		}
+	}
+
+	// One at a time
+	private void AdvancePatientQueue() {
+		GameObject emptySeat = null;
+		// Looking for an empty welcome seat
+		for (int i = 0; i < WelcomeSeats.Length; i++) {
+			if (!WelcomeSeats[i].GetComponent<SeatController>().isHolding) {
+				emptySeat = WelcomeSeats[i];
+				break;
+			}
+		}
+
+		for (int i = 0; i < PatientQueue.Length; i++) {
+			if (PatientQueue[i] != null) {
+				if(emptySeat != null) {
+					PatientQueue[i].transform.parent = null;
+					PatientQueue[i].transform.position = emptySeat.transform.position;
+					PatientQueue[i].transform.rotation = emptySeat.transform.rotation;
+					emptySeat.GetComponent<SeatController>().ReceiveHold(PatientQueue[i]);
+					emptySeat = null;
+					PatientQueue[i] = null;
+				}
+			} else {
+				// Empty place in queue, can someone advance?
+				for(int j = i+1; j < PatientQueue.Length; j++) {
+					if(PatientQueue[j] != null) {
+						PatientQueue[i] = PatientQueue[j];
+						PatientQueue[j] = null;
+						PatientQueue[i].transform.localPosition = new Vector3(0, i, 0);
+						break;
+					}
+                }
+            }
+		}
+	}
+
+	private bool TrySpawnNewPatient() {
+		GameObject emptySeat = null;
+		// Looking for an empty welcome seat
+		for (int i = 0; i < WelcomeSeats.Length; i++) {
+			if (!WelcomeSeats[i].GetComponent<SeatController>().isHolding) {
+				emptySeat = WelcomeSeats[i];
+				break;
+			}
+		}
+
+		GameObject newPatient = null;
+		// An empty welcome seat was found
+		if (emptySeat != null) {
+			newPatient = Instantiate(Patient, emptySeat.transform.position, emptySeat.transform.rotation);
+			newPatient.name = "patient " + counter++;
+			newPatient.GetComponent<PatientController>().Initialize(this);
+			emptySeat.GetComponent<SeatController>().ReceiveHold(newPatient);
+
+			return true;
+		} else {
+			// No welcome seat, try to add to queue
+			for (int i = 0; i < PatientQueue.Length; i++) {
+				if (PatientQueue[i] == null) {
+					// Nice slot you got there mate
+					PatientQueue[i] = Instantiate(Patient, PatientQueueParent);
+					PatientQueue[i].transform.localPosition = new Vector3(0, i, 0);
+					PatientQueue[i].name = "patient " + counter++;
+					PatientQueue[i].GetComponent<PatientController>().Initialize(this);
+
+					return true;
+				}
+			}
+		}
+
+		return false;
 	}
 
 	private void CheckNewPlayers() {
@@ -78,7 +136,12 @@ public class GameController : MonoBehaviour {
 		GameObject newPlayer = Instantiate(Player);
 		newPlayer.GetComponent<PlayerController>().Initialize(id, this, playerSpeed);
 		Players.Add(id, newPlayer);
-		isPlaying = true;
+
+		if(!isPlaying) {
+			// The queue will try to advance each half second, starting now
+			InvokeRepeating("AdvancePatientQueue", 0f, .5f);
+			isPlaying = true;
+		}
 
 		currentSpawnRate = spawnRate / ((Players.Count + 1)/2f);
 		elapsedTime = currentSpawnRate;
