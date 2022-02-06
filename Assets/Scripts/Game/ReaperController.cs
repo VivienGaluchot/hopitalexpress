61e69f1f337d65b144c2d676b400e4bda6e58216
Vivien Galuchot <vivien.galuchot@gmail.com>
2022-02-06 15:58:51 +0100

    random walk

---
+++ Assets/Scripts/Game/ReaperController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReaperController : MonoBehaviour
+{
+
+	private Rigidbody2D r2d;
+
+	private float decisionPeriod = 2;
+
+	private float counter = 0;
+
+	private Vector2 currentDecision;
+
+
+	void Start() {
+		r2d = GetComponent<Rigidbody2D>();
+		currentDecision = new Vector2(0, 0);
+	}
+
+	void FixedUpdate() {
+		r2d.velocity = currentDecision;
+	}
+
+	void Update() {
+		counter += Time.deltaTime;
+		if (counter > decisionPeriod) {
+			NewDecision();
+			counter = 0;
+		}
+	}
+
+	private void NewDecision() {
+		int random = Random.Range(0, 10);
+		switch (random) {
+			case 0:
+				currentDecision.x += .5f;
+				break;
+			case 1:
+				currentDecision.x -= .5f;
+				break;
+			case 2:
+				currentDecision.y += .5f;
+				break;
+			case 3:
+				currentDecision.y -= .5f;
+				break;
+			case 4:
+				currentDecision.Set(0, 0);
+				break;
+	   }
+	   currentDecision = Vector2.ClampMagnitude(currentDecision, 1.5f);
+	}
+}
