fedd13bd2c34f401e9fdbefc7adbbc076ebd49db
Vivien Galuchot <vivien.galuchot@gmail.com>
2022-02-20 18:28:22 +0100

    rename

--- Assets/Scripts/Menu/LevelSelectionController.cs
+++ Assets/Scripts/Menu/LevelSelectionController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class LevelSelectionCtontroller : MonoBehaviour {
+public class LevelSelectionController : MonoBehaviour {
 
 	public GameObject playerPrefab;
 
